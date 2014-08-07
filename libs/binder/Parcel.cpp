47b810c0871c918f9a6f6296bcb6cb5357a88ba5
Inge Wallin <inge@lysator.liu.se>
2014-08-07 17:26:13 +0700

    Undo an unnecessary change.

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1079,7 +1079,7 @@ String16 Parcel::readString16() const
 {
     size_t len;
     const char16_t* str = readString16Inplace(&len);
-    if (str) return String16(str, size_t(len));
+    if (str) return String16(str, len);
     ALOGE("Reading a NULL string not supported here.");
     return String16();
 }
