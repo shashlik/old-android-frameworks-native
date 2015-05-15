db1e0a1ae3692ec6500db6d518483ecbfd40f14c
Dan Leinir Turthra Jensen <admin@leinir.dk>
2015-05-15 14:10:36 +0100

    No, this should be there. Otherwise parcel doesn't work

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1170,7 +1170,7 @@ String16 Parcel::readString16() const
 {
     size_t len;
     const char16_t* str = readString16Inplace(&len);
-//     if (str) return String16(str, len);
+    if (str) return String16(str, len);
     ALOGE("Reading a NULL string not supported here.");
     return String16();
 }
